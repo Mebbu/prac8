0deef100cc2fc3784c03e689f2099d3898f799d6
Madison Berg <madison.berg@students.mq.edu.au>
2021-10-06 18:23:03 +1100

    PLayers move independently
    hell yeah

--- COMP2160 Prac Week 8/Assets/Scripts/Player.cs
+++ COMP2160 Prac Week 8/Assets/Scripts/Player.cs
@@ -11,20 +11,55 @@ public class Player : MonoBehaviour
     [SerializeField]
     private float speed;
 
+    private string thisPlayer;
+
+    private string Vertical;
+    private string Horizontal;
+
     void Start()
     {
-        
+        if (CompareTag("Player"))
+        {
+            //thisPlayer = "p1"
+            Vertical = InputAxes.Vertical;
+            Horizontal = InputAxes.Horizontal;
+        }
+        else if (CompareTag("Player2"))
+        {
+            //thisPlayer = "p2"
+            Vertical = InputAxes.Vertical_2;
+            Horizontal = InputAxes.Horizontal_2;
+        }
     }
 
-    // Update is called once per frame
     void Update()
+
     {
-        vx = Input.GetAxis("Horizontal");
-        vz = Input.GetAxis("Vertical");
+        //if(p1)
+        Move(Horizontal, Vertical);
+
+        //If Player 2
+        //Move(Vertical_2, Horizontal_2);
+    }
+
+    void Move(string moveX, string moveY)
+    {
+        vx = Input.GetAxis(moveX);
+        vz = Input.GetAxis(moveY);
 
         Direction = new Vector3(vx, 0, vz);
         velocity = Direction * speed;
         transform.Translate(velocity * Time.deltaTime);
 
     }
+
+    
+}
+
+class InputAxes
+{
+    public const string Vertical = "Vertical";
+    public const string Horizontal = "Horizontal";
+    public const string Vertical_2 = "Vertical_2";
+    public const string Horizontal_2 = "Horizontal_2";
 }
